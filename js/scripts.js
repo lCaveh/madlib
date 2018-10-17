816a206858d692f000ec0d96fa13381274fcadc8
Sandra Castaneda <sancastans@gmail.com>
2018-10-17 10:28:28 -0700

    added jquery bootstrap and updated madlibs content'

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
-    $(".animal").append("blah blah");
-    $(".exclamation").append("blah blah");
-    $(".verb").append("blah blah");
-    $(".noun").append("blah blah");
+  $("#formOne").submit(function(event) {
+    var adjective1Input = $("input#adjective1").val();
+    var hobbie1Input = $("input#hobbie1").val();
+    var hobbie2Input= $("input#hobbie2").val();
+    var hobbie3Input = $("input#hobbie3").val();
+    var foodInput = $("input#food").val();
+    var adjective2Input = $("input#adjective2").val();
+
+    $(".adjective1").text(adjective1Input);
+    $(".hobbie1").text(hobbie1Input);
+    $(".hobbie2").text(hobbie2Input);
+    $(".hobbie3").text(hobbie3Input);
+    $(".food").text(foodInput);
+    $(".adjective2").text(adjective2Input);
 
     $("#story").show();
+
+    event.preventDefault();
   });
 });

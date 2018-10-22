a91ac51ee0bb394571362995f3cab33185147c75
Kaveh Saleminejad and Sandra Castaneda <kaveh@>
2018-10-22 08:55:28 -0700

    add array and loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,10 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var adjective1Input = $("input#adjective1").val();
-    var hobbie1Input = $("input#hobbie1").val();
-    var hobbie2Input= $("input#hobbie2").val();
-    var hobbie3Input = $("input#hobbie3").val();
-    var foodInput = $("input#food").val();
-    var adjective2Input = $("input#adjective2").val();
-
-    $(".adjective1").text(adjective1Input);
-    $(".hobbie1").text(hobbie1Input);
-    $(".hobbie2").text(hobbie2Input);
-    $(".hobbie3").text(hobbie3Input);
-    $(".food").text(foodInput);
-    $(".adjective2").text(adjective2Input);
-
+    var blanks = ["adjective1","hobbie1","hobbie2","hobbie3","food","adjective2"];
+    blanks.forEach(function (blank) {
+      var userInput = $("input#"+blank).val();
+      $("."+blank).text(userInput);
+    });
     $("#story").show();
 
     event.preventDefault();
